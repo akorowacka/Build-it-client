353351b4fa64236cdf9a23df7be761cc428e6a4e
Patryk <patryk.dom15@gmail.com>
2016-04-11 21:47:27 +0200

    Small landing page animation

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,8 +1,13 @@
 app.config(function($stateProvider , $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 
-    $stateProvider.state('home' , {
-        url: '/',
-        templateUrl:'app/views/home.html'
-    });
+    $stateProvider
+        .state('home' , {
+            url: '/',
+            templateUrl:'app/views/home.html'
+        })
+        .state('list', {
+            url: '/list',
+            templateUrl:'app/views/list.html'
+        });
 });
